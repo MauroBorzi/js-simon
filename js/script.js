e3cbd4a4f4adfd157f946c1be7e8ab08a5db3544
MauroBorzi <mauroborzi91@gmail.com>
2025-07-11 15:13:12 +0200

    [add]randomNumbers

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 const countdown = document.getElementById(`countdown`)
 const numbersList = document.getElementById(`numbers-list`)
 const answersForm = document.getElementById(`answers-form`)
+const instructions = document.getElementById(`instructions`)
 
 // definisco una variabile che mi conta i secondi
 let seconds = 5
@@ -13,7 +14,8 @@ const intervalId = setInterval(function () {
     clearInterval(intervalId)
     answersForm.classList.remove(`d-none`)
     countdown.classList.add(`d-none`)
-
+    numbersList.classList.add(`d-none`)
+    instructions.innerText = `Inserisci i numeri memorizzati! (In qualsiasi ordine)`
   }
   else {
     countdown.innerText = seconds
@@ -22,3 +24,15 @@ const intervalId = setInterval(function () {
   seconds--
 
 }, 1000)
+
+// funzione per generale i numeri casuali
+function randomNumber() {
+  for (let i = 0; i < 5; i++) {
+    const randomNumbers = Math.floor(Math.random() * 50) + 1
+    const listNumber = document.createElement("li")
+    listNumber.textContent = randomNumbers
+    numbersList.appendChild(listNumber)
+  }
+}
+
+randomNumber();
